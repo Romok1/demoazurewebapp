24adcfbb31f0748a43dfa630f02bcc3165d57f37
Romok1 <ajayirommy@yahoo.com>
2021-04-21 22:18:21 +0100

    updated startup.cs file

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace demoazurewebapp
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello Azure!");
                 });
             });
         }
